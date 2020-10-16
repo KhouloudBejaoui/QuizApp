d632902392c888838fbf4d61d40b400c188cfb29
khouloud711 <bejaouikhouloud98@gmail.com>
2020-10-16 12:00:46 +0200

    Updates

---
+++ precache-manifest.ad7555d4f7765b80618317c1ff8577bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b879288d002782a047d2251703011b13",
+    "url": "/QuizApp/index.html"
+  },
+  {
+    "revision": "fb798547db506cd1edac",
+    "url": "/QuizApp/static/css/main.bd9eb63f.chunk.css"
+  },
+  {
+    "revision": "ed9ea346e3ff48e375c0",
+    "url": "/QuizApp/static/js/2.4ad7820e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/QuizApp/static/js/2.4ad7820e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb798547db506cd1edac",
+    "url": "/QuizApp/static/js/main.f2f3736f.chunk.js"
+  },
+  {
+    "revision": "553ecaa52dc12743a190",
+    "url": "/QuizApp/static/js/runtime-main.ca1486b2.js"
+  }
+]);
